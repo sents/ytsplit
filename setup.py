b662eb99fe7c1fa8ec4af12af148be9277f775e7
Finn Krein <finnkrein@googlemail.com>
2019-05-08 15:47:02 +0200

    Made a package out of the script

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = 'ytsplit',
+    version = '0.1',
+    description = 'A script to download and split up videos from youtube',
+    long_description = \
+    '''
+    This script is supposed to be used to import audios from youtube to a music collection,
+    splitting up the audion at times given in a timstamp string as used on youtube
+    ''',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    license = 'The Unlicense',
+    install_requires = ['youtube-dl'],
+    author = 'sents',
+    author_email = 'finn@krein.moe',
+    url = 'https://github.com/sents/ytsplit',
+    packages = ['ytsplit'],
+    entry_points = { 'console_scripts' : ['ytsplit = ytsplit.ytsplit:main'] }
+)
