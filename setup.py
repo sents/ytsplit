910f8414b39e019d1c1c0704f29c4f041a01f341
Finn Krein <finnkrein@googlemail.com>
2020-02-11 02:57:21 +0100

    Add option to give a structure string
    This structure string can be used to  have arbitrary formats of
    tracklists as long as the identifiers (hours, minutes...) are
    separated by a constant string.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = 'ytsplit',
-    version = '0.1',
+    version = '0.2',
     description = 'A script to download and split up videos from youtube',
     long_description = \
     '''
--- ytsplit/ytsplit.py
+++ ytsplit/ytsplit.py
@@ -9,21 +9,30 @@ from tempfile import TemporaryDirectory
 import argparse
 
 
-def songtimes(timestring, structure_string=None, killindex=True):
+def songtimes(timestring, structure_string=None, killindex=True, delimiter="\n"):
     if structure_string is not None:
-        timesep, strips = parse_structure(structure_string)
+        parsf = parse_structure(structure_string)
+        structure_dicts = [parsf(i) for i in timestring.split(delimiter)]
+        titles = [d[r"\t"] for d in structure_dicts]
+        timestamps = [
+            (d.get("\\h", 0), d.get("\\m", 0), d.get("\\s", 0)) for d in structure_dicts
+        ]
     else:
         timesep = (
-            r"(?:\d{1,2}:)?(?:\d{1,2}):(?:\d{1,2})"
-        )  # Regex to split at (hh:)mm:ss
-    timestamps = [re.split(":", m.group()) for m in re.finditer(timesep, timestring)]
+            r"(?:\d{1,2}:)?(?:\d{1,2}):(?:\d{1,2})"  # Regex to split at (hh:)mm:ss
+        )
+        titles = [s.strip(" \n") for s in re.split(timesep, timestring) if s != ""]
+        timestamps = [
+            re.split(":", m.group()) for m in re.finditer(timesep, timestring)
+        ]
     timestamps = [
         sum(int(t) * 1000 * 60 ** s for s, t in enumerate(reversed(times)))
         for times in timestamps
     ]
     times = zip(timestamps, timestamps[1:] + [None])
-    titles = filter(lambda x: x != "", re.split(timesep, timestring))
-    titles = list(map(lambda s: s.strip(" \n"), titles))
+    # titles = filter(lambda x: x != "", re.split(timesep, timestring)) # you can tell
+    # that I had just learned about map and filter :D
+    # titles = list(map(lambda s: s.strip(" \n"), titles))
     if killindex:
         titles = [re.sub(r"\d{1,2}(\. | - |\))", "", title) for title in titles]
     times = [t for n, t in enumerate(times) if titles[n] != "!junk!"]
@@ -31,11 +40,87 @@ def songtimes(timestring, structure_string=None, killindex=True):
     return times, titles
 
 
+structure_keys = {
+    r"\h",  # hours
+    r"\m",  # minutes
+    r"\s",  # seconds
+    r"\t",  # title
+    r"\n",  # number
+}
+
+replace_rules = {
+    r"\h": r"\d{1,3}",
+    r"\m": r"\d{1,3}",
+    r"\s": r"\d{1,3}",
+    r"\t": r".*",
+    r"\n": r"\d{1,3}",
+}
+
+key_pattern = "(" + "|".join(map(re.escape, structure_keys)) + ")"
+
+
+def ids_positions(ipattern):
+    return [i.span() for i in re.finditer(key_pattern, ipattern)]
+
+
+def gap_positions(positions, start, end):
+    inds = [(positions[i][1], positions[i + 1][0]) for i in range(len(positions) - 1)]
+    if positions[0][0] > start:
+        inds = [(start, positions[0][0])] + inds
+    if positions[-1][1] < end:
+        inds = inds + [(positions[-1][1], end)]
+    return inds
+
+
+def slice_with_list(inp, slist):
+    return [inp[slice(*pos)] for pos in slist]
+
+
+def parse_structure(ipattern):
+    r"""
+        Creates a function which gets a Title and timestamp from a string,
+        given an input of a pattern like these:
+        \n. \m:\s - \t$
+        or
+        \n) \h - \m - \s | \t$
+        or just
+        \t \m:\s$
+    """
+    id_pos = ids_positions(ipattern)
+    gap_pos = gap_positions(id_pos, 0, len(ipattern))
+    id_patterns = slice_with_list(ipattern, id_pos)
+    gap_patterns = slice_with_list(ipattern, gap_pos)
+    id_patterns_escaped = (r"({})".format(replace_rules[id_i]) for id_i in id_patterns)
+    gap_patterns_escaped = (re.escape(s) for s in gap_patterns)
+    pattern_number = len(id_patterns) + len(gap_patterns)
+    ordered_patterns = []
+    order = 0 if id_pos[0][0] == 0 else 1
+    for i in range(pattern_number):
+        if (i + order) % 2 == 0:
+            ordered_patterns.append(next(id_patterns_escaped))
+        else:
+            ordered_patterns.append(next(gap_patterns_escaped))
+    match_pattern = "".join(ordered_patterns)
+
+    def sparser(istring):
+        match = re.match(match_pattern, istring).groups()
+        matchdict = {p: m for p, m in zip(id_patterns, match)}
+        return matchdict
+
+    return sparser
+
+
 def splitytsong(
-    tlist, url, directory, artist="unknown", album="unknown", etags={}, ftime=False
+    mseclist,
+    namelist,
+    url,
+    directory,
+    artist="unknown",
+    album="unknown",
+    etags={},
+    ftime=False,
 ):
     cwd = str(Path.cwd())
-    mseclist, namelist = songtimes(tlist)
     ydl_opts = {
         "format": "bestaudio/best",
         "postprocessors": [
@@ -96,6 +181,28 @@ def main():
         default=None,
         help="Tracklist like popular on youtube. If not provided uses stdin",
     )
+    parser.add_argument(
+        "-x",
+        "--delimiter",
+        default="\n",
+        help=r"Delimiter of the different tracks in the tracklist (defaults to newline)",
+    )
+    parser.add_argument(
+        "-s",
+        "--structure",
+        default=None,
+        help=r"""String that gives the structure of the tracklist
+        It can have following identifiers:
+
+        \h : hours
+        \m : minutes
+        \s : seconds
+        \t : trackname
+        \n : tracknumber
+
+        Every identifier needs to be separated from
+        the next one by a constant delimiter""",
+    )
 
     args = parser.parse_args()
 
@@ -110,15 +217,19 @@ def main():
     else:
         tracklist = args.tracklist
 
+    mseclist, namelist = songtimes(
+        tracklist, structure_string=args.structure, delimiter=args.delimiter
+    )
+
     if args.test:
-        mseclist, namelist = songtimes(tracklist)
         print("\n".join(namelist))
         return 0
     elif args.url is None:
         print("Please provide url to download.")
 
     splitytsong(
-        tracklist,
+        mseclist,
+        namelist,
         args.url,
         str(Path(args.directory)),
         artist=args.interpret,
